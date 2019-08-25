78f2deb105f28cf2203f654de1ac75601dad723b
superff <superffeng@gmail.com>
2019-08-25 15:23:17 -0700

    Update the program to accept ctrl c

--- StringCalculator/StringCalculator/Program.cs
+++ StringCalculator/StringCalculator/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
+using System.Text;
 
 namespace StringCalculator
 {
     class Program
     {
+        private static bool run = true;
+        private static StringBuilder sb = new StringBuilder();
+        private static ICalculator calculator = new StringCalculator();
+
         public static void Main(string[] args)
         {
-            string val;
-            Console.Write("Enter String: ");
-            val = Console.ReadLine();
+            // Establish an event handler to process key press events.
+            Console.CancelKeyPress += cancelHandler;
+            Console.WriteLine("Enter String To Calulate");
+            Console.WriteLine("press CTRL+C to end:");
+            while (run)
+            {
+                sb.AppendLine(Console.ReadLine());
+            }
+        }
 
-            ICalculator calculator = new StringCalculator();
-            calculator.Calculate(val);
+        protected static void cancelHandler(object sender, ConsoleCancelEventArgs args)
+        {
+            run = false;
+
+            // calculate the string input
+            calculator.Calculate(sb.ToString());
         }
     }
 }
